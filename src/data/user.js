09f0e0bc34bf659e501cb159d298d7241dc8be44
The Anand <anands2959@gmail.com>
2024-07-22 22:14:53 +0530

    first commit

---
+++ src/data/user.js
@@ -0,0 +1,85 @@
+const INFO = {
+	main: {
+		title: "The Anand",
+		name: "Anand Kumar Sharma",
+		email: "anands2959@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/anands2959",
+		github: "https://github.com/anands2959",
+		linkedin: "https://www.linkedin.com/in/anand-kumar-sharma-ab9837216/",
+		instagram: "https://www.instagram.com/thisiz_the_anand/",
+		codepen: "https://codepen.io/Anand-Kumar-Sharma",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Full-stack web and mobile app developer, and web designer",
+		description:
+			"I am a full stack developer with expertise in the MERN stack and mobile app design. I build scalable, secure applications and enjoy solving complex problems. Many of my projects are open-source, and I'm always open to new challenges and collaborations.",
+	},
+
+	about: {
+		title: "I’m Anand Kumar Sharma from India, Where I design the future.",
+		description:
+			"I am a full stack developer with expertise in the MERN stack. I have experience in building scalable, secure, and reliable web applications, as well as designing user-friendly mobile apps. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. Many of my projects are open-source and available for others to explore and contribute to. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "NodeJS Snippets extension of VS code",
+			description:
+				"A collection of useful code snippets for Node.js development. Each snippet includes a prefix for quick access and a description of its functionality.",
+			logo: "https://w7.pngwing.com/pngs/452/24/png-transparent-js-logo-node-logos-and-brands-icon.png",
+			linkText: "View Project",
+			link: "https://marketplace.visualstudio.com/items?itemName=anandcoder.nodejs-snippets-by-the-anand",
+		},
+		{
+			title: "Api Request extension of VS code",
+			description:
+				"API Request Tester is a Visual Studio Code extension that allows you to send HTTP requests and view responses directly within the editor.",
+			logo: "https://w7.pngwing.com/pngs/452/24/png-transparent-js-logo-node-logos-and-brands-icon.png",
+			linkText: "View Project",
+			link: "https://github.com/anands2959/api-request-by-the-anand?tab=readme-ov-file",
+		},
+
+		{
+			title: "Image Generation by using DALL-E 3",
+			description:
+				"Developed an image generation website using DALL-E 3 to generate images based on user prompts. The site displays the generated images on the body of the webpage and allows users to download the images.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/anands2959/api-request-by-the-anand?tab=readme-ov-file",
+		},
+
+		{
+			title: "Signature Dashboard",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/anands2959/Signature-Dashboard",
+		},
+
+		
+
+		{
+			title: "Programming language C",
+			description:
+				"Developed Two mini project in C language Hotel Bill generator and hostel room allotment",
+			logo: "https://cdn.jsdelivr.net/npm/@programming-languages-logos/c@0.0.3/c_256x256.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
