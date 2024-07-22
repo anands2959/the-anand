3580f7a4fa775342c6b7f623e5edeeb936c91186
The Anand <91383238+anands2959@users.noreply.github.com>
2024-07-22 23:55:26 +0530

    Update seo.js

--- src/data/seo.js
+++ src/data/seo.js
@@ -2,36 +2,36 @@ const SEO = [
 	{
 		page: "home",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"I am a full stack developer with expertise in the MERN stack and mobile app design. I build scalable, secure applications and enjoy solving complex problems. Many of my projects are open-source, and I'm always open to new challenges and collaborations.",
+		keywords: ["TheAnand", "theanand", "the anand", "Anand Sharma"],
 	},
 
 	{
 		page: "about",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"I am a full stack developer with expertise in the MERN stack. I have experience in building scalable, secure, and reliable web applications, as well as designing user-friendly mobile apps. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. Many of my projects are open-source and available for others to explore and contribute to. I am always looking for new challenges and opportunities to grow as a developer.",
+		keywords: ["TheAnand", "theanand", "the anand", "Anand Sharma"],
 	},
 
 	{
 		page: "articles",
 		description:
 			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+		keywords: ["TheAnand", "theanand", "the anand", "Anand Sharma"],
 	},
 
 	{
 		page: "projects",
 		description:
-			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"I am a full stack developer with expertise in the MERN stack. I have experience in building scalable, secure, and reliable web applications, as well as designing user-friendly mobile apps. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. Many of my projects are open-source and available for others to explore and contribute to. I am always looking for new challenges and opportunities to grow as a developer.",
+		keywords: ["TheAnand", "theanand", "the anand", "Anand Sharma"],
 	},
 
 	{
 		page: "contact",
 		description:
 			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+		keywords: ["TheAnand", "theanand", "the anand", "Anand Sharma"],
 	},
 ];
 
